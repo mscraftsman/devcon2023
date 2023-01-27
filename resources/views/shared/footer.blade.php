772480099414e423e37cd12c17562937b1d0addb
Ish Sookun <ish.sookun@lasentinelle.mu>
2023-01-27 11:27:37 +0400

    Did some merge clean-up

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,28 +1,5 @@
 @vite(['resources/scss/app.scss'])
 
-<<<<<<< HEAD
-<div class="absolute -mt-32 right-0">
-    <img class="w-[360px]" src="/images/png/astronaut-flag.png" />
-</div>
-
-<div class="mt-16 absolute w-full bg-[#000] h-[220px] skew-y-6"></div>
-
-<div class="absolute w-full bg-[#000] mt-44 pb-24 grid place-items-center">
-    <img class="w-20" src="/images/svg/mscc-logo.svg" />
-    <h3 class="mt-2 text-center text-xl text-mustard font-astronomus uppercase">
-        Mauritius Software Craftsmanship Community
-    </h3>
-
-    <nav class="mt-8 flex flex-row gap-6 text-sm text-gray-100 uppercase">
-        <a class="hover:text-mustard transition hover:ease-in-out duration-300" href="{{ route('code-of-conduct') }}">Code of conduct</a>
-        <a class="hover:text-mustard transition hover:ease-in-out duration-300" href="https://drive.google.com/file/d/1GQY5WElMktXO7JLxgYJq8tWDuKVFra9A/view?usp=share_link">Become a sponsor</a>
-        <a class="hover:text-mustard transition hover:ease-in-out duration-300 hidden" href="#">Extras</a>
-        <a class="hover:text-mustard transition hover:ease-in-out duration-300 hidden" href="#">Our Squad Team</a>
-    </nav>
-</div>
-
-</footer>
-=======
 <footer class="space--footer">
 	<div class="footer--bg-1">
 		<img src="/images/SVG/footer-bg-1.svg" alt="">
@@ -34,7 +11,7 @@
 		<div class="container--fw">
 			<img src="/images/SVG/mscc-logo-dark.svg" alt="" class="footer-logo">
 			<div class="mscc--text">Mauritius Software Craftsmanship Community</div>
-			
+
 			<div class="list--menu">
 				<ul>
 					<li><a href="{{ route('code-of-conduct') }}">Code of conduct</a></li>
@@ -46,4 +23,3 @@
 		</div>
 	</div>
 </footer>
->>>>>>> 80bd5dd1970802c961d99e0e6d8ac9a39ef2e8aa
