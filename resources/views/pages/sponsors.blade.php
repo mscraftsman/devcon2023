659f290d30f4601f002fa00c52ad685a8688a797
Tejas Pagooah <tejas@pagooah.com>
2023-03-28 23:19:27 +0400

    + Ringier logo added + IOS Ltd logo added + Code of conduct page text updated

--- resources/views/pages/sponsors.blade.php
+++ resources/views/pages/sponsors.blade.php
@@ -8,128 +8,136 @@
 													 		url({{ asset('images/svg/flag.svg') }}),
 													 		url({{ asset('images/svg/satellite.svg') }})">
 
-        <h2 class="mt-5 text-center text-gray-600 font-bold font-spotnik uppercase">Our valued allies</h2>
-        <h1 class="mt-2 pb-5 mb-10 text-mustard text-center text-4xl font-astronomus uppercase">Sponsors & Partners</h1>
+	<h2 class="mt-5 text-center text-gray-600 font-bold font-spotnik uppercase">Our valued allies</h2>
+	<h1 class="mt-2 pb-5 mb-10 text-mustard text-center text-4xl font-astronomus uppercase">Sponsors & Partners</h1>
 
-        <!-- Diamond Tier -->
+	<!-- Diamond Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/mercury.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Mercury <span class="package font-dm">(Diamond Tier)</span></p>
-            <img class="w-12" src="/images/png/mercury.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/mercury.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Mercury <span class="package font-dm">(Diamond Tier)</span></p>
+			<img class="w-12" src="/images/png/mercury.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper mercury--sponsors mb-10">
-					<div class="sponsor py-8 px-8">
-						<a href="https://www.mcb.mu" target="_blank">
-							<img src="{{ asset('images/sponsors/mcb.png') }}" />
-						</a>
-					</div>
-				</div>
+	<div class="sponsors-logo-wrapper mercury--sponsors mb-10">
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.mcb.mu" target="_blank">
+				<img src="{{ asset('images/sponsors/mcb.png') }}" />
+			</a>
+		</div>
+	</div>
 
-        <!-- Titanium Tier -->
+	<!-- Titanium Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/venus.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Venus <span class="package font-dm">(Titanium Tier)</span></p>
-            <img class="w-12" src="/images/png/venus.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/venus.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Venus <span class="package font-dm">(Titanium Tier)</span></p>
+			<img class="w-12" src="/images/png/venus.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper venus--sponsors mb-10">
-					<div class="sponsor py-8 px-8">
-						<a href="https://www.sdworx.co.uk/en-gb/mauritius" target="_blank">
-							<img src="{{ asset('images/sponsors/sdworx.svg') }}" />
-						</a>
-					</div>
-					<div class="sponsor medium py-8 px-8">
-						<a href="https://www.providus.mu/" target="_blank">
-							<img src="{{ asset('images/sponsors/providus.png') }}" />
-						</a>
-					</div>
-				</div>
+	<div class="sponsors-logo-wrapper venus--sponsors mb-10">
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.sdworx.co.uk/en-gb/mauritius" target="_blank">
+				<img src="{{ asset('images/sponsors/sdworx.svg') }}" />
+			</a>
+		</div>
+		<div class="sponsor medium py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.providus.mu/" target="_blank">
+				<img src="{{ asset('images/sponsors/providus.png') }}" />
+			</a>
+		</div>
+	</div>
 
-        <!-- Platinum Tier -->
+	<!-- Platinum Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/earth.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Earth <span class="package font-dm">(Platinum Tier)</span></p>
-            <img class="w-12" src="/images/png/earth.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/earth.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Earth <span class="package font-dm">(Platinum Tier)</span></p>
+			<img class="w-12" src="/images/png/earth.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper earth--sponsors mb-10">
-					<div class="sponsor py-8 px-8">
-						<a href="https://www.swanforlife.com/en" target="_blank">
-							<img src="{{ asset('images/sponsors/swan.png') }}" />
-						</a>
-					</div>
-				</div>
+	<div class="sponsors-logo-wrapper earth--sponsors mb-10">
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.swanforlife.com/en" target="_blank">
+				<img src="{{ asset('images/sponsors/swan.png') }}" />
+			</a>
+		</div>
+	</div>
 
-        <!-- Gold Tier -->
+	<!-- Gold Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/mars.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Mars <span class="package font-dm">(Gold Tier)</span></p>
-            <img class="w-12" src="/images/png/mars.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/mars.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Mars <span class="package font-dm">(Gold Tier)</span></p>
+			<img class="w-12" src="/images/png/mars.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper mars--sponsors mb-10">
-					<div class="sponsor large--high py-8 px-8">
-						<a href="https://www.klanik.com/" target="_blank">
-							<img src="{{ asset('images/sponsors/klanik.png') }}" />
-						</a>
-					</div>
-					<div class="sponsor small py-8 px-8">
-						<a href="https://www.elca.ch/en/elca-mauritius" target="_blank">
-							<img src="{{ asset('images/sponsors/elca.png') }}" />
-						</a>
-					</div>
-				</div>
+	<div class="sponsors-logo-wrapper mars--sponsors mb-10">
+		<div class="sponsor large--high py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.klanik.com/" target="_blank">
+				<img src="{{ asset('images/sponsors/klanik.png') }}" />
+			</a>
+		</div>
+		<div class="sponsor small py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.elca.ch/en/elca-mauritius" target="_blank">
+				<img src="{{ asset('images/sponsors/elca.png') }}" />
+			</a>
+		</div>
+	</div>
 
-        <!-- Silver Tier -->
+	<!-- Silver Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/jupiter.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Jupiter <span class="package font-dm">(Silver Tier)</span></p>
-            <img class="w-12" src="/images/png/jupiter.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/jupiter.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Jupiter <span class="package font-dm">(Silver Tier)</span></p>
+			<img class="w-12" src="/images/png/jupiter.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper jupiter--sponsors mb-10">
-					<div class="sponsor small py-8 px-8">
-						<a href="https://www.novity.io/" target="_blank">
-							<img src="{{ asset('images/sponsors/novity.svg') }}" />
-						</a>
-					</div>
-					<!-- <div class="sponsor py-8 px-8">
-						<a href="https://www.novity.io/" target="_blank">
-							<img src="{{ asset('images/sponsors/clarity.svg') }}" />
-						</a>
-					</div> -->
-				</div>
+	<div class="sponsors-logo-wrapper jupiter--sponsors mb-10">
+		<div class="sponsor small py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.novity.io/" target="_blank">
+				<img src="{{ asset('images/sponsors/novity.svg') }}" />
+			</a>
+		</div>
+	</div>
 
-        <!-- Bronze Tier -->
+	<!-- Bronze Tier -->
 
-        <div class="mt-16 title--container">
-            <img class="w-12" src="/images/png/saturn.png" />
-            <p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Saturn <span class="package font-dm">(Bronze Tier)</span></p>
-            <img class="w-12" src="/images/png/saturn.png" />
-        </div>
+	<div class="mt-16 title--container">
+			<img class="w-12" src="/images/png/saturn.png" />
+			<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Saturn <span class="package font-dm">(Bronze Tier)</span></p>
+			<img class="w-12" src="/images/png/saturn.png" />
+	</div>
 
-				<div class="sponsors-logo-wrapper saturn--sponsors mb-10">
-					<div class="sponsor small py-8 px-8">
-						<a href="https://www.maccs.mu/" target="_blank">
-							<img src="{{ asset('images/sponsors/maccs.svg') }}" />
-						</a>
-					</div>
-					<div class="sponsor py-8 px-8 large">
-						<a href="https://www.maccs.mu/" target="_blank">
-							<img src="{{ asset('images/sponsors/clarity.png') }}" />
-						</a>
-					</div>
-				</div>
+	<div class="sponsors-logo-wrapper saturn--sponsors mb-10">
+		<div class="sponsor small py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.maccs.mu/" target="_blank">
+				<img src="{{ asset('images/sponsors/maccs.svg') }}" />
+			</a>
+		</div>
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8 large">
+			<a href="https://www.maccs.mu/" target="_blank">
+				<img src="{{ asset('images/sponsors/clarity.png') }}" />
+			</a>
+		</div>
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8 small">
+			<a href="https://www.ios.mu/" target="_blank">
+				<img src="{{ asset('images/sponsors/ios.png') }}" />
+			</a>
+		</div>
+	</div>
 
-    <!-- <div class="">
-        <img class="ml-32 w-[200px]" src="/images/svg/big-bang.svg" />
-        <img class="pt-28 ml-4 w-[160px]" src="/images/svg/flag.svg" />
-        <img class="pt-10 ml-32 w-[200px]" src="/images/svg/satellite.svg" />
-    </div> -->
+	<!-- Speaker Tier -->
+
+	<div class="mt-16 title--container no--planets">
+		<p class="text-xl text-center text-gray-600 font-bold font-astronomus uppercase package--line">Speaker <span class="package font-dm">(Sponsor)</span></p>
+	</div>
+
+	<div class="sponsors-logo-wrapper speaker--sponsors mb-10">
+		<div class="sponsor py-5 px-5 md:py-8 md:px-8">
+			<a href="https://www.ringier.com/" target="_blank">
+				<img src="{{ asset('images/sponsors/ringier.png') }}" />
+			</a>
+		</div>
+	</div>
 </section>
