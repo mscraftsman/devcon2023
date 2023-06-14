cab2753137cf8ec0841e6e18bb265c91e6df344f
Tejas Pagooah <tejas@pagooah.com>
2023-06-14 22:19:09 +0400

    Minor improvements

--- resources/js/agenda.js
+++ resources/js/agenda.js
@@ -1,12 +1,8 @@
 window.onscroll = function() { affixedRooms() };
 
-// Get the header
-var header = document.getElementById("rooms-bar");
+let header = document.getElementById("rooms-bar");
+let sticky = header.getBoundingClientRect().top + document.documentElement.scrollTop;
 
-// Get the offset position of the navbar
-var sticky = header.getBoundingClientRect().top + document.documentElement.scrollTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function affixedRooms() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
@@ -15,13 +11,19 @@ function affixedRooms() {
   }
 }
 
-var container = document.querySelector('.sessions__column');
-var fixedElements = document.querySelectorAll('.time__tag');
+let container = document.querySelector('.sessions__column');
+let fixedElements = document.querySelectorAll('.time__tag');
 
 container.addEventListener('scroll', function () {
-  var scrollLeft = container.scrollLeft;
+  let scrollLeft = container.scrollLeft;
 
   fixedElements.forEach(function(fixedElement) {
     fixedElement.style.transform = 'translateX(' + scrollLeft + 'px) translateY(-30px)';
   });
+
+  if (window.pageYOffset > sticky) {
+    header.style.transform = 'translateX(' + -scrollLeft + 'px)';
+  } else {
+    header.style.transform = 'none';
+  }
 });
