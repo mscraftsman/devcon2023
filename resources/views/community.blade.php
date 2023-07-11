b43d33d68915ad708992c78364d4fb27109073df
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-07-12 00:32:38 +0400

    chore: Add pymmug info

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -15,4 +15,36 @@
 		<p class="mb-2">GDGs are local groups of developers who are specifically interested in Google products and APIs. Each local group is called a GDG chapter and can host a variety of technical activities for developers - from just a few people getting together to watch our latest videos, to large gatherings with demos and tech talks, to hackathons.</p>
 		<p>Disclaimer: GDG Mauritius is an independent user group; our activities and the opinions expressed here should in no way be linked to Google, the corporation.</p>
 	</section>
+	<section class="mt-10 max-w-7xl px-3 mx-auto bg-gradient-to-r from-gray-200 to-gray-200 px-5 py-5 rounded-lg drop-shadow-xl mb-20">
+		<h2 class="mt-1 mb-2 text-gray-700 text-2xl font-astronomus">Python Mauritius User Group</h2>
+		<p>
+          The Python Mauritius User Group (pymug) is the local Python user group
+          of Mauritius. Our aim is to provide opportunities to everybody to
+          upskill their level of Python and to ease industry-adoption of
+          Python-related technologies. A nice overview of the User Group can be
+          found on the MSCC blog
+          <a href="https://www.mscc.mu/python-mauritius-usergroup-pymug/"
+            >here</a
+          >. To sum it up, we care about Python education. Our website:
+          <a href="https://www.pymug.com">pymug.com</a>
+        </p>
+
+        <p>
+          We'll be present at the DevCon which is an amazing community-driven
+          event.
+        </p>
+
+        <p>
+          The Pymug organises year-round events, topic days, meetups, from
+          scratch sessions as well as university topic coverage. Have a look at
+          our
+          <a href="https://www.pymug.com/assets/pymug_2019_report.pdf">2019</a>
+          or
+          <a href="https://www.pymug.com/assets/pymug_end_of_year_2020_v2.pdf"
+            >2020</a
+          >
+          end of year reports. Looking forward to meet you ✨.
+        </p>
+	</section>
+	
 @endsection
